8f7fa6d332ad96cec7288d85b2b31b702465a5c2
Екатерина Василевич <katerinair8@mail.ru>
2023-03-07 00:08:00 +0100

    branches

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import sys
 from os.path import abspath, dirname, join
 
-root_dir = dirname(abspath(__file__))
+root_dir = dirname(dirname(abspath(__file__)))
 sys.path.append(root_dir)
 infra_dir_path = join(root_dir, 'infra')
 
